84abca2b7f6c133d08dc1681b6284289cb788c31
Andrew Miller <andrewmiller0@gmail.com>
2015-01-13 18:12:09 -0500

    Switching drivers to deal with the schema issue

--- client/app/store/store.controller.js
+++ client/app/store/store.controller.js
@@ -8,23 +8,52 @@ angular.module('stackStoreApp')
 		$scope.newItem = {quantity:1};
 
 		this.openModal = function(item) {
-			console.log(item)
 			$scope.currentItem = item;
-			$modal.open({
+			$scope.modal = $modal.open({
 				templateUrl:'../../components/modal/itemModal.html',
 				scope: $scope
 			})
 		};
 
-		this.addToCart = function(item) {
-			
+		this.addToCart = function(item, optionsObj) {
+			//find current user
+			var user = Auth.getCurrentUser();
+			//create temporary frontend object to send as req.body
+			var tempLineItem = {
+				item: [item],
+				sender: [user],
+				receiverName: optionsObj.receiverName,
+				receiverEmail: optionsObj.receiverEmail,
+				message: optionsObj.message,
+				quantity: optionsObj.quantity,
+				value: 20, //replace with actual value
+				themeURL: item.image
+			}
 			// create a line item on the server
+			$http.post('/api/lineItems/', tempLineItem).
+				success(function(data, status){
+					console.log('Data = ' + data);
+					console.log('Status = ' + status);
+				}).
+				error(function(data, status){
+					console.log('Error Data = ' + data);
+					console.log('Error Status = ' + status);
+				});
+
+
+
+
+			//if user has a cart: add to cart
+
+
+			//if user does not have a cart, create cart and add line item
+
 
 			// add that line item to the cart of the current user
 
 			//
 
-
+			$scope.modal.close(); //this works
 		}
 
 	});
--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -5,9 +5,19 @@ var mongoose = require('mongoose'),
 
 var LineItem = require('../lineItem/lineItem.model');
 
+// var OrderSchema = new Schema({
+//   orderItems: Array,
+//   status: String
+// });
+
+
 var OrderSchema = new Schema({
-  orderItems: [LineItem],
-  status: String
+  status: String,
+  orderItems:[LineItem]
 });
 
-module.exports = mongoose.model('Order', OrderSchema);
+
+// module.exports = mongoose.model('Order', OrderSchema);
+
+
+module.exports = OrderSchema;
