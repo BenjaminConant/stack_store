b29084b86ce23d1a2cc2fd6fe661ee26650d0bcc
Sam Corcos <samcorcos@gmail.com>
2015-01-14 18:35:06 -0500

    items being created in db - ben and sam

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -6,6 +6,25 @@ angular.module('stackStoreApp')
     // Use the User $resource to fetch all users
     $scope.users = User.query();
 
+    $scope.title = "";
+    $scope.image = "";
+    $scope.description = "";
+    $scope.message = "";
+    $scope.categories = [];
+
+    $scope.submitItem = function() {
+      $http.post('api/items', {
+        title: $scope.title,
+        image: $scope.image,
+        description: $scope.description,
+        defaultMessage: $scope.message,
+        categories: $scope.categories
+      }).success(function(err, item) {
+        if (err) console.log(err);
+        console.log(item);
+      })
+    }
+
     $scope.delete = function(user) {
       User.remove({ id: user._id });
       angular.forEach($scope.users, function(u, i) {
