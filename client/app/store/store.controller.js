a8bc75ec7a252b1231303a0ff6720a576bef9a3e
Griffin <g.telljohann@gmail.com>
2015-01-15 10:58:18 -0500

    fixing the cart to populate things
    Andrew Miller & Griffin Telljohann

--- client/app/store/store.controller.js
+++ client/app/store/store.controller.js
@@ -2,78 +2,85 @@
 
 angular.module('stackStoreApp')
 
-	.controller('StoreCtrl', function($scope, item, $http, $modal, Auth) {
-		var self = this;
-		self.allItems = item.query();
+.controller('StoreCtrl', function($scope, item, $http, $modal, Auth) {
+  var self = this;
+  self.allItems = item.query();
 
-		var user = Auth.getCurrentUser();
+  var user = Auth.getCurrentUser();
 
-		// if (user.$promise) {
-		// 	console.log('User has a promise!');
-		// 	user.$promise.then(function(currentUser){
+  // if (user.$promise) {
+  // 	console.log('User has a promise!');
+  // 	user.$promise.then(function(currentUser){
 
-		// 			$http.get('/api/orders/' + currentUser.cart)
-		// 			.success(function(data){
-		// 				self.cart = data.orderItems;
-		// 			});
-		// 	});
-		// } else {
-		// 	console.log("User doesn't have a promise")
-		// 	$http.get('/api/orders/' + user.cart)
-		// 	.success(function(data){
-		// 		self.cart = data.orderItems;
-		// 	});
-		// }
+  // 			$http.get('/api/orders/' + currentUser.cart)
+  // 			.success(function(data){
+  // 				self.cart = data.orderItems;
+  // 			});
+  // 	});
+  // } else {
+  // 	console.log("User doesn't have a promise")
+  // 	$http.get('/api/orders/' + user.cart)
+  // 	.success(function(data){
+  // 		self.cart = data.orderItems;
+  // 	});
+  // }
 
-		//console.log(user);
-		//console.log(user.cart);
+  //console.log(user);
+  //console.log(user.cart);
 
-		var currentItem = {};
-		$scope.newItem = {quantity:1};
+  var currentItem = {};
+  $scope.newItem = {
+    quantity: 1
+  };
 
-		this.openModal = function(item) {
-			$scope.currentItem = item;
-			$scope.modal = $modal.open({
-				templateUrl:'../../components/modal/itemModal.html',
-				scope: $scope
-			})
-		};
+  this.openModal = function(item) {
+    $scope.currentItem = item;
+    $scope.modal = $modal.open({
+      templateUrl: '../../components/modal/itemModal.html',
+      scope: $scope
+    })
+  };
 
 
-    this.addToCart = function(item, optionsObj) {
-      //find current user
-      var user = Auth.getCurrentUser();
-      //create temporary frontend object to send as req.body
-      var tempLineItem = {
-        item: item._id,
-        sender: user._id,
-        receiverName: optionsObj.receiverName,
-        receiverEmail: optionsObj.receiverEmail,
-        message: optionsObj.message,
-        quantity: optionsObj.quantity,
-        value: 20//, //replace with actual value later
+  this.addToCart = function(item, optionsObj) {
+    //find current user
+    var user = Auth.getCurrentUser();
+    //create temporary frontend object to send as req.body
+    var tempLineItem = {
+      item: item._id,
+      sender: user._id,
+      receiverName: optionsObj.receiverName,
+      receiverEmail: optionsObj.receiverEmail,
+      message: optionsObj.message,
+      quantity: optionsObj.quantity,
+      value: 20 //, //replace with actual value later
         // themeURL: item.image
-      }
-    
-
-      //if user has a cart: add to cart ONLY DO THIS IN FIRST STEP
-
-
-
-      //if user does not have a cart, create cart and add line item
-
-
-      // add that line item to the cart of the current user
-      //Push line item to users cart
-      //
-
-      $http.post('/api/lineItems/', tempLineItem);
-        // .success(function(data, status) {})
-        // .error(function(data, status) {
-        //   console.log('Error Data = ' + data);
-        //   console.log('Error Status = ' + status);
-        // });
-      $scope.modal.close(); //this works
     }
 
-  });
+
+    //if user has a cart: add to cart ONLY DO THIS IN FIRST STEP
+
+
+
+    //if user does not have a cart, create cart and add line item
+
+
+    // add that line item to the cart of the current user
+    //Push line item to users cart
+    //
+
+    $http.post('/api/lineItems/', tempLineItem)
+      .success(function(newLineItem, status) {
+        //newLineItem.item = item;
+        $scope.orderItems.push(newLineItem);
+        $scope.cartTotal += newLineItem.value;
+      });
+ 
+      // .error(function(data, status) {
+      //   console.log('Error Data = ' + data);
+      //   console.log('Error Status = ' + status);
+      // });
+    $scope.modal.close(); //this works
+  }
+
+});
