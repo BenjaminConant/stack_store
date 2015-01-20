b79bdc8ccd37f939fb607fe1134d7b89c8686cfe
Sam Corcos <samcorcos@gmail.com>
2015-01-20 12:06:18 -0500

    reviews are submitting properly

--- client/app/account/accountInfo/accountInfo.controller.js
+++ client/app/account/accountInfo/accountInfo.controller.js
@@ -1,13 +1,16 @@
 'use strict';
 
 angular.module('stackStoreApp')
-  .controller('AccountInfoCtrl', function ($scope, Auth, $http) {
+  .controller('AccountInfoCtrl', function ($scope, Auth, $http, $modal) {
 
     $scope.currentUser = Auth.getCurrentUser();
-    $scope.orders = [];
+    $scope.currentItem;
+    $scope.review = {};
+
 
     $scope.getOrders = function() {
       $http.get('/api/users/'+$scope.currentUser._id + '/orders').success(function(orders) {
+        $scope.orders = [];
         orders.forEach(function(order) {
           if (order.status !== "cart") {
             order.total = 0;
@@ -20,6 +23,27 @@ angular.module('stackStoreApp')
       })
     }
     $scope.getOrders();
+
+    $scope.openModal = function(item) {
+      $scope.currentItem = item;
+      $scope.modal = $modal.open({
+        templateUrl: '../../components/modal/reviewModal.html',
+        scope: $scope
+      })
+    };
+
+    $scope.submitReview = function() {
+      console.log("getting called");
+      $scope.review.author = $scope.currentUser._id;
+      $scope.review.item = $scope.currentItem._id;
+      $http.post('/api/reviews', $scope.review).success(function(review) {
+        $scope.review.stars = '';
+        $scope.review.text = '';
+        alert("Successfully added to database!")
+        console.log(review);
+      })
+      $scope.modal.close();
+    }
 
     // $scope.orders = [];
     //
